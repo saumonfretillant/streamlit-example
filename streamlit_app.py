d5af1ff90a689b62c391c0f956e82ba222fc06eb
saumonfretillant <116170716+saumonfretillant@users.noreply.github.com>
2022-11-10 13:41:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from collections import namedtuple
 import altair as alt
-import math
 import pandas as pd
 import streamlit as st
 
