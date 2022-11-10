cc76a9733e5803228e5f6fdf8eca5486c8731f65
saumonfretillant <116170716+saumonfretillant@users.noreply.github.com>
2022-11-10 22:22:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,13 +48,14 @@ vectorizer = vect()
 if st.button("Detecter le sujet d'insatisfaction"):
     st.write('L\'avis choisi : ')
     st.write(text)
+    st.write('')
     polarity, topics_list = fonction_prediction(model,vectorizer,number,text)
     topics =""
     for topic in topics_list:
         topics += topic+' , '
     topics = topics[0:-3]
     if genre == 'Avis du dataset':
-        st.write("les topics de l'avis sont : ",topics)
+        st.write("les topics de l'avis sont : ",*topics*)
     else:
-        st.write("polarité du nouvel avis : ",polarity,"les topics de l'avis sont : ",topics)
+        st.write("polarité du nouvel avis : ",polarity,"les topics de l'avis sont : ",*topics*)
 
