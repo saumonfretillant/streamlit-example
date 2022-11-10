78c7d51603722ddc2c0054bba788dd8e04886ffb
saumonfretillant <116170716+saumonfretillant@users.noreply.github.com>
2022-11-10 14:43:47 +0100

    Update text_dataset.py

--- text_dataset.py
+++ text_dataset.py
@@ -1,7 +1,18 @@
 
 import pandas as pd
 DATASET_FILE = "dataset.csv"
+VECTORIZER_FILE = "vectorizer"
+MODEL_FILE = "model"
+import pickle
 
 def text_dataset(index):
     df = pd.read_csv(DATASET_FILE)
     return(df.text[index])
+
+def load(file):
+    with (open(file, "rb")) as f:
+        while True:
+            try:
+                model = pickle.load(f)
+            except EOFError:
+                break
