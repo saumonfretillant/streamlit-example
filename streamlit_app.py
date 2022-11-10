f48acf18aa9689698e07fc08456bef2281c52050
saumonfretillant <116170716+saumonfretillant@users.noreply.github.com>
2022-11-10 13:03:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,31 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
 from collections import namedtuple
 import altair as alt
 import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+categories_count = ['1', '2', '3']
+chosen_count = st.selectbox(
+    'Choisir le nombre de topic !',
+    categories_count
+)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+text= st.text_input("Entrez un texte:")
+if st.button("Detecter le sujet d'insatisfaction"):
+    st.write('detecter le sujet')
+number = st.number_input('Insert a number',min_value=1,max_value=15,step=1)
+st.write('The current number is ', number)
+genre = st.sidebar.radio(
+    "Quel Texte Analyser ?",
+    ('Avis dataset', 'Texte Libre'))
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if genre == 'Avis dataset':
+    st.write('You selected comedy.')
+else:
+    st.write("You didn't select comedy.")
