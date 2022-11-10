b8165118728e1131bd9e9a90059e9d1497ccbab2
saumonfretillant <116170716+saumonfretillant@users.noreply.github.com>
2022-11-10 21:28:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,8 @@ import pandas as pd
 DATASET_FILE = "dataset.csv"
 import pickle
 
-@st.cache()
+session_state = st.session_state['text']
+
 def text_dataset(index):
     df = pd.read_csv(DATASET_FILE)
     return(df.text[index])
@@ -30,13 +31,16 @@ def model():
 
 genre = st.sidebar.radio('Quel Texte Analyser ?',('Avis dataset', 'Texte Libre'))
 if genre == 'Avis dataset':
+    session_state['text']=''
     number = st.sidebar.number_input('Choisir le numéro de l\'index',min_value=1,max_value=10000,step=1)
     if st.sidebar.button('Prédire un avis via le numéro d\'index'):
         text = text_dataset(number-1)
+        session_state['text']=text
         st.sidebar.write(text)
     if st.sidebar.button('Prédire un avis aléatoire'):
         random = rd.randint(0,9999)
         text = text_dataset(random)
+        session_state['text']=text
         st.sidebar.write(text)
 else:
     text= st.sidebar.text_input("Entrez un nouvel avis:")
@@ -61,16 +65,16 @@ except:
     st.write("Pas d'avis séléctionné")
 
 st.write("-------------")
-st.write(text)
+st.write(session_state['text'])
 st.write("-------------")
 
 if st.button("Detecter le sujet d'insatisfaction"):
     st.write(number)
-    st.write(text)
+    st.write(session_state['text'])
     if text =="":
         st.write("Pas d'avis séléctionné")
     else:
-        st.write(text)
+        st.write(session_state['text'])
         #st.write(fonction_prediction(model(),vect(),number,text))
 
 
