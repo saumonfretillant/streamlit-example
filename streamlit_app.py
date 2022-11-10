1d40a1200c0566dd44229289de1c849cb098f123
saumonfretillant <116170716+saumonfretillant@users.noreply.github.com>
2022-11-10 14:08:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@
 import altair as alt
 import pandas as pd
 import streamlit as st
+from fonctions.py import test
 
 genre = st.sidebar.radio('Quel Texte Analyser ?',('Avis dataset', 'Texte Libre'))
 if genre == 'Avis dataset':
