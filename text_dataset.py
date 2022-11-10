46895f84a978c1f885d15e3e51b356a568c85b55
saumonfretillant <116170716+saumonfretillant@users.noreply.github.com>
2022-11-10 15:15:05 +0100

    Update text_dataset.py

--- text_dataset.py
+++ text_dataset.py
@@ -2,8 +2,7 @@
 import pandas as pd
 DATASET_FILE = "dataset.csv"
 import pickle
-import sklearn
-from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 def text_dataset(index):
     df = pd.read_csv(DATASET_FILE)
